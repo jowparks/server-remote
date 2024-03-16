cd4ae95d7d53d68f4d76d86592eeccf7c0da259e
Joe <joe@iflabs.network>
2024-03-15 22:54:56 -0700

    add modal

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import * as SecureStore from 'expo-secure-store';
-import { ChevronRight } from '@tamagui/lucide-icons'
-import { Button, ListItem, Separator, View, YGroup } from 'tamagui';
+import { ChevronRight, Plus } from '@tamagui/lucide-icons'
+import { Button, Card, H2, H3, H4, ListItem, Separator, Spacer, View, YGroup } from 'tamagui';
 
 
 import SSHClient from '@jowparks/react-native-ssh-sftp';
+import AddServerModal from './modal';
 
 interface Server {
     host: string;
@@ -36,6 +37,7 @@ async function getItem<K extends keyof StorageItems>(key: K): Promise<StorageIte
 
 export default function ServerSelectScreen() {
     const [servers, setServers] = useState<Server[]>([]);
+    const [addServerOpen, setAddServerOpen] = useState(false);
 
     useEffect(() => {
         // Load servers from AsyncStorage
@@ -54,22 +56,27 @@ export default function ServerSelectScreen() {
     };
 
     return (
-        <View flex={1} alignItems="center">
-            <YGroup alignSelf="center" bordered width={240} size="$5" separator={<Separator />}>
-                {servers.map((server,index) => (
-                    <YGroup.Item key={server.host+index}>
-                        <ListItem
-                            hoverTheme
-                            pressTheme
-                            title={server.host}
-                            iconAfter={ChevronRight}
-                        />
-                    </YGroup.Item>
+        <View flex={1} alignItems="center" style={{ padding: 20 }}>
+            <Spacer size="10%" />
+                {servers.map((server, index) => (
+                    // <YGroup.Item key={server.host+index}>
+                    //     <ListItem
+                    //         hoverTheme
+                    //         pressTheme
+                    //         title={server.host}
+                    //         iconAfter={ChevronRight}
+                    //     />
+                    // </YGroup.Item>
+                  <Card elevate size="$4" bordered key={server.host+index} style={{ marginBottom: 20, width: '90%' }}>
+                    <Card.Header padded style={{ padding: 10 }}>
+                        <H4>{server.host}</H4>
+                    </Card.Header>
+                  </Card>
                 ))}
-            </YGroup>
-            <Button onPress={() => addServer({ host: '192.168.1.51', port: 123, user: 'foo'})} >
-                Add Server
+            <Button hoverTheme pressTheme onPress={() => setAddServerOpen(true)} style={{ marginTop: 20, width: '90%' }} >
+                <Plus />
             </Button>
+            <AddServerModal open={addServerOpen} onOpenChange={setAddServerOpen} onAddServer={() => {}} />
         </View>
     );
 }
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,3 +1,45 @@
-export default function ModalScreen() {
-  return null
+import React, { useState } from 'react';
+import { Box, Button, Input, Sheet } from 'tamagui';
+
+export type ServerData = { host: string, port: string, user: string, password?: string, key?: string, passphrase?: string, name?: string };
+
+type AddServerModalProps = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  onAddServer: (server: ServerData) => void; 
+};
+
+export default function AddServerModal({ onAddServer, open, onOpenChange }: AddServerModalProps) {
+  const [position, setPosition] = useState(0)
+  const [serverDetails, setServerDetails] = useState<ServerData>({ host: '', port: '', user: '', password: '', key: '', passphrase: '', name: '' });
+
+  const handleAddServer = () => {
+    onAddServer(serverDetails);
+    onOpenChange(false);
+  };
+  return (
+    <Sheet
+    forceRemoveScrollEnabled={open}
+    modal={true}
+    open={open}
+    onOpenChange={onOpenChange}
+    snapPoints={[85, 50, 25]}
+    snapPointsMode={'percent'}
+    dismissOnSnapToBottom
+    position={position}
+    onPositionChange={setPosition}
+    zIndex={100_000}
+    // animation="medium"
+  >
+    <Sheet.Overlay
+      animation="lazy"
+      enterStyle={{ opacity: 0 }}
+      exitStyle={{ opacity: 0 }}
+    />
+    <Sheet.Handle />
+    <Sheet.Frame padding="$4" justifyContent="center" alignItems="center" space="$5">
+      <Input width={200} />
+    </Sheet.Frame>
+  </Sheet>
+  );
 }
